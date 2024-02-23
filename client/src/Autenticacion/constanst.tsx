e80f1ea6f8076f7b730f2eb7d7546284178415ec
marialuisaalonso850 <marialuisaalonso850@gmail.com>
2024-02-23 07:07:26 -0500

    nuevo

--- client/src/Autenticacion/constanst.tsx
+++ client/src/Autenticacion/constanst.tsx
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
 export const API_URL = "https://parqueaderorolesback.onrender.com/api";
-=======
-export const API_URL = "https://parqueaderorolesback.onrender.com/api";
->>>>>>> dc74ce3a1d3601d807f8ba37c71a800d21cb2485
